987babdcd5338e231784acdd7311a16cc4fffee4
Sarvesh <SarveshKumar9839545008>
2024-02-09 21:45:44 +0530

    git init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//Import something from 'somewhere'
+import React from "react";
+import Tophead from "./components/Tophead/Tophead.js";
+
+
+//Function Defination Area
+function App() {
+  return (
+    <>
+      <Tophead />
+      <header/>
+    </>
+  );
+}
+
+//Exporting Area
+export default App;
---
+++ src/components/Tophead/Tophead.js
@@ -0,0 +1,20 @@
+//Import something from "somewhere"
+import React from "react";
+import './Tophead.css'
+
+//Function Defination Area
+function  Tophead(){
+    return(
+        <>
+           <div className="tophead">
+               <div className="left">Welcome Guest</div>
+               <div className="right">Stay Upload</div>
+           </div>
+        </>
+    )
+}
+
+
+
+//Exporting Area
+export default Tophead;
